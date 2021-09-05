6b6a1424c71fc6ff8898f862f9039bc1fe29507d
thatFemicode <leyedavid10@gmail.com>
2021-09-05 15:51:52 +0100

    author functionality

---
+++ routes/authors.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name !== null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i');
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render('authors/index', { authors: authors, searchOptions: req.query });
+  } catch {
+    res.redirect('/');
+  }
+});
+
+// New Author Routes
+// Pass Variables from author model and these variables are going to be sent
+// down to  the ejs file but this does not say anything to the database but it creates an author which we can
+// Use to save delete and update things in out ejs file
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() });
+});
+
+// Route to CREATE THE AUTHORS WRITE TO POST the authors to the route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    res.redirect('authors');
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Erroer Creating Author',
+    });
+  }
+  //   First Version
+  //   author.save((err, newAuthor) => {
+  //     if (err) {
+  //       res.render('authors/new', {
+  //         author: author,
+  //         errorMessage: 'Erroer Creating Author',
+  //       });
+  //     } else {
+  //       res.redirect('authors');
+  //     }
+  //   });
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -6,21 +6,31 @@
 //   require('dotenv').config();
 // }
 
-require('dotenv').config();
-const uri = process.env.MONGODB_URI;
-console.log(uri);
+// require('dotenv').config();
+// const uri = process.env.MONGODB_URI;
+// console.log(uri);
+
 const express = require('express');
+// Require dotenv for the environment varaibles
+const dotenv = require('dotenv');
+const path = require('path');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-
+const bodyParser = require('body-parser');
+dotenv.config({ path: '.env' });
+const PORT = process.env.PORT || 3000;
+const connectDB = require('./database/connection');
+connectDB();
 // Hook Up initial index router to the server by requiring it
 const indexRouter = require('./routes/index');
-
+const authorRouter = require('./routes/authors');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+// Layput file imports everything from all our pages (views)
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 // const uri =
 //   'mongodb+srv://user:tumininu10@cluster0.kagsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
@@ -29,23 +39,26 @@ const mongoose = require('mongoose');
 
 // Using out connection to connect to either a development or production database
 
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB Connected!');
-  })
-  .catch((err) => {
-    console.log('DB Connection Error: ' + err);
-  });
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// .then(() => {
+//   console.log('DB Connected!');
+// })
+// .catch((err) => {
+//   console.log('DB Connection Error: ' + err);
+// });
 // // Checking if connection is pure now
 // mongoose.connection.once('open', () => {
 //   console.log('connected!');
 // });
-// mongoose.connection.on('error', (error) => console.error(error));
-mongoose.connection.once('open', () => console.log('Connected to database'));
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database'));
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
