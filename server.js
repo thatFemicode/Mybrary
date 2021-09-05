229019a97f0eef657af55242674d74e5a2deb4e5
thatFemicode <leyedavid10@gmail.com>
2021-09-05 01:45:57 +0100

    uri parameter

--- server.js
+++ server.js
@@ -9,7 +9,6 @@
 require('dotenv').config();
 const uri = process.env.DATABASE_URL;
 console.log(uri);
-
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -22,6 +21,8 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+// const uri =
+//   'mongodb+srv://user:tumininu10@cluster0.kagsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 // Connecting mongodb library with the app
 const mongoose = require('mongoose');
@@ -33,7 +34,10 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB Connected!'))
+  .then(() => {
+    console.log('DB Connected!');
+    app.listen(3000);
+  })
   .catch((err) => {
     console.log('DB Connection Error: ' + err);
   });
@@ -44,4 +48,3 @@ mongoose
 // mongoose.connection.on('error', (error) => console.error(error));
 mongoose.connection.once('open', () => console.log('Connected to database'));
 app.use('/', indexRouter);
-app.listen(process.env.PORT || 3000);
